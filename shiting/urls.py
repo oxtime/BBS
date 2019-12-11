8cd481f15d0cb75d7fcfb6c796788ba8be615441
insnce <1064620936@qq.com>
2019-12-11 18:59:40 +0800

    1.2

--- shiting/urls.py
+++ shiting/urls.py
@@ -1,4 +1,4 @@
-"""在线试听 URL Configuration
+"""shiting URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
